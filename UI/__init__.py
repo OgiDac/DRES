37e95e5c7a613d9847a9fa247fab414a6a7f1d46
OgiDac <ognjendacevic2000@gmail.com>
2023-01-31 00:34:19 +0100

    Bug

--- UI/__init__.py
+++ UI/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8c9fa3d6ed31570f45b6084b'
 
 login_manager = LoginManager(app)
-login_manager.login_view = "profileView"
+login_manager.login_view = "index"
 login_manager.login_message_category = "info"
 login_manager.init_app(app)
 
